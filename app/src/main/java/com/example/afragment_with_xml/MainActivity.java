1f15c61e665a44be074c32a54a2c48ff4e246548
SinaSystem <sinasystem47@gmail.com>
2020-06-04 21:35:44 +0430

    Initial commit

---
+++ app/src/main/java/com/example/afragment_with_xml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.afragment_with_xml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
